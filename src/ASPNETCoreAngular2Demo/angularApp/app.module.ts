fe4020a2c9d40a03359ef55bb1f2b32eada413f4
FabianGosebrink <fabian.gosebrink@outlook.com>
2017-07-15 18:08:15 +0200

    latest versions

--- src/ASPNETCoreAngular2Demo/angularApp/app.module.ts
+++ src/ASPNETCoreAngular2Demo/angularApp/app.module.ts
@@ -1,14 +1,15 @@
-import { HomeModule } from './home/home.module';
-import { AboutModule } from './about/about.module';
-import { SharedModule } from './shared/shared.module';
-import { CoreModule } from './core/core.module';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { AppRouting } from './app.routes';
 import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
 import { ToasterModule } from 'angular2-toaster';
 import { LoadingBarModule } from 'ng2-loading-bar';
+
+import { AboutModule } from './about/about.module';
+import { AppComponent } from './app.component';
+import { AppRouting } from './app.routes';
+import { CoreModule } from './core/core.module';
+import { HomeModule } from './home/home.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
     imports: [
